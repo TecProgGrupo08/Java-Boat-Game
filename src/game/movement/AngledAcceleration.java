aab9cf93a7a90c7488242dc73ad17342557e4313
Marco Kuroiva <marco.antoniorl10@gmail.com>
2016-10-25 20:11:11 -0200

    Prioritizing clarity over concision on AngledAcceleration

--- src/game/movement/AngledAcceleration.java
+++ src/game/movement/AngledAcceleration.java
@@ -57,14 +57,21 @@ public class AngledAcceleration extends Movement {
 
         double angle = getAngle();
         double angularVelocity = getAngularVelocity();
-        angle += angularVelocity;
+        angle = angle + angularVelocity;
         angle = clampAngle(angle);
         
         /*
          * Changes the angle of the boat (if going left or right)
          */
-        xVelocity = Math.cos(angle) * velocity;
-        yVelocity = Math.sin(angle) * velocity;
+        double cosine;
+        double sin;
+        
+        cosine = Math.cos(getAngle());
+        sin = Math.sin(getAngle());
+
+        
+        xVelocity = cosine * velocity;
+        yVelocity = sin * velocity;
 
         setAngle(angle);
         setAngularVelocity(angularVelocity);
@@ -117,12 +124,12 @@ public class AngledAcceleration extends Movement {
 
     private double damp(double value, double factor) {
         if (value > 0.0) {
-            value -= factor;
+            value = value - factor;
             if (value < 0.0) {
                 value = 0.0;
             }
         } else {
-            value += factor;
+            value = value + factor;
             if (value > 0.0) {
                 value = 0.0;
             }
@@ -158,7 +165,7 @@ public class AngledAcceleration extends Movement {
 
     private void recalculateAngle() {
         double angle = getAngle();
-        angle += getAngularVelocity();
+        angle = angle + getAngularVelocity();
         angle = clampAngle(angle);
         setAngle(angle);
     }
@@ -216,13 +223,25 @@ public class AngledAcceleration extends Movement {
 
     private void recalculateXVelocity() {
         double xVelocity;
-        xVelocity = Math.cos(getAngle()) * getVelocity();
+        double cosine;
+        double velocity;
+        
+        cosine = Math.cos(getAngle());
+        velocity = getVelocity();
+        
+        xVelocity = cosine * velocity;
         setXVelocity(xVelocity);
     }
 
     private void recalculateYVelocity() {
         double yVelocity;
-        yVelocity = Math.sin(getAngle()) * getVelocity();
+        double sin;
+        double velocity;
+        
+        sin = Math.sin(getAngle());
+        velocity = getVelocity();
+        
+        yVelocity = sin * velocity;
         setYVelocity(yVelocity);
     }
 }
