0598d5ca05b37edac6b8408371716bd455d61b4f
Anderson Silva <andersoonn.ams@gmail.com>
2016-11-05 16:39:44 -0200

    Applying logging on Buoy.java

--- src/game/sprite/Buoy.java
+++ src/game/sprite/Buoy.java
@@ -4,47 +4,64 @@ import game.movement.Location;
 import game.character.Character;
 import java.awt.*;
 
-public class Buoy extends Vector
-{
+import org.apache.log4j.Logger;
+
+public class Buoy extends Vector {
+    
+	
+	static Logger logging = Logger.getLogger(Character.class); //basic log system
+	
     Color mainColor= new Color(20,50,24);
  
-    public Buoy(Character owner)
-    {
-	super(owner);
+    public Buoy(Character owner) {
+		
+		super(owner);
+    
     }
     
-    public Graphics paint(Graphics g, Location loc)
-    {
+    public Graphics paint(Graphics g, Location loc) {
 	
-	int x = this.getTransformedArea().getBounds().x;
-	int y = this.getTransformedArea().getBounds().y;
-	Graphics2D gg = (Graphics2D) g;
-	if (isSpriteShown())
-	{
-	    gg.setColor(Color.BLUE);
-	    //loc.setLocation(x,y);
-	    gg.setColor(Color.BLACK);
-	    gg.fillOval(x-1,y-2,20+2,20+1);
-	    gg.setColor(new Color(255,50,34));
-	    gg.fillOval(x,y,20,20);
-	    gg.setColor(new Color(250,250,250));
-	    gg.fillOval(x+5,y+5,20-8,20-8);
-	    gg.setColor(new Color(255,50,34));
-	    gg.fillOval(x+7,y+7,20-15,20-15);
-	    
-	}
-	
-	return (Graphics)gg;
+    	logging.info("Colorindo campo.");
+		int x = this.getTransformedArea().getBounds().x;
+		logging.debug("The windown with is: " + x);
+		int y = this.getTransformedArea().getBounds().y;
+		logging.debug("The windown hight is: " + y);
+		Graphics2D gg = (Graphics2D) g;
+		
+		if (isSpriteShown()){
+			
+			logging.info("Sprite was load seccessfully.");
+		    gg.setColor(Color.BLUE);
+		    //loc.setLocation(x,y);
+		    gg.setColor(Color.BLACK);
+		    gg.fillOval(x-1,y-2,20+2,20+1);
+		    gg.setColor(new Color(255,50,34));
+		    gg.fillOval(x,y,20,20);
+		    gg.setColor(new Color(250,250,250));
+		    gg.fillOval(x+5,y+5,20-8,20-8);
+		    gg.setColor(new Color(255,50,34));
+		    gg.fillOval(x+7,y+7,20-15,20-15);
+		    
+		} else{
+			
+			//do nothing
+			logging.info("Cant load Sprite.");
+		}
+		
+		return (Graphics)gg;
     }
     
-    public Color getMainColor()
-    {
-	return mainColor;
+    public Color getMainColor() {
+		
+		return mainColor;
+    
     }
     
-    public void setMainColor(Color obstacleMainColor)
-    {
-	this.mainColor = obstacleMainColor;
+    public void setMainColor(Color obstacleMainColor){
+    	
+    	logging.debug("Obstacle color is: " + obstacleMainColor);
+		this.mainColor = obstacleMainColor;
+		
     }
     
 }
