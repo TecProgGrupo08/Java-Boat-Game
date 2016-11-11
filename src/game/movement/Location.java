a132d83dad9016f084171a11615e9d38f45bf91e
Anderson Silva <andersoonn.ams@gmail.com>
2016-11-11 00:00:03 -0200

    Fixing identation on some classes

--- src/game/movement/Location.java
+++ src/game/movement/Location.java
@@ -5,62 +5,49 @@
 
 package game.movement;
 
-public class Location
-{
+public class Location {
 
-    
-    private double x;
-    private double y;
+	private double x;
+	private double y;
 
+	/** Creates a new instance of Location */
+	public Location(double x, double y) {
+		this.x = x;
+		this.y = y;
 
-    /** Creates a new instance of Location */
-    public Location(double x, double y)
-    {
-	this.x=x;
-	this.y=y;
+	}
 
-    }
-    
-    public Location(int x, int y)
-    {
-	this.x=(double)x;
-	this.y=(double)y;
-	
-    }
-    @Override
-    public String toString(){
-    return (String)("x:"+String.valueOf(x)+" y:"+String.valueOf(y));
-}
-    public double getX()
-    {
-        return x;
-    }
+	public Location(int x, int y) {
+		this.x = (double) x;
+		this.y = (double) y;
 
-    public void setX(double x)
-    {
-        this.x = x;
-    }
+	}
 
-    
+	@Override
+	public String toString() {
+		return (String) ("x:" + String.valueOf(x) + " y:" + String.valueOf(y));
+	}
 
-    public double getY()
-    {
-        return y;
-    }
+	public double getX() {
+		return x;
+	}
 
-    
+	public void setX(double x) {
+		this.x = x;
+	}
 
-    public void setY(double y)
-    {
-	    this.y = y;
-    }
-    
-    public void setLocation(double x, double y){
-	this.x=x;
-	this.y=y;
+	public double getY() {
+		return y;
+	}
 
-    }
-    
+	public void setY(double y) {
+		this.y = y;
+	}
 
+	public void setLocation(double x, double y) {
+		this.x = x;
+		this.y = y;
+
+	}
 
 }
