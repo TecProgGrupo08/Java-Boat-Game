c4255606143e7fe44437814c87ac5bdd10fd824f
Anderson Silva <andersoonn.ams@gmail.com>
2016-11-26 12:20:41 -0200

    Commenting structures on Goal class

--- src/game/character/Goal.java
+++ src/game/character/Goal.java
@@ -8,25 +8,33 @@ import game.GameEngine;
 import java.util.*;
 class Goal extends Stationary {
 	
-	//If the colision is with the goal you win
+	//If the colision is the goal you win
     @Override
     public boolean detectCollision(ArrayList<Character> moving) {
         boolean collision = false;
         int length = moving.size();
         
+        //iterates the length of the movement 
         for (int i = 0; i < length; i++) {
+        	
             Character character = moving.get(i);
             boolean detectCollision = collides(character);
-            if (collision = detectCollision) { //checks if the boat hits the goal
+            
+            //check if occurred collision on the current move
+            if (collision = detectCollision) { 
+            	
+            	//check if this collision is between harboar and boat 
                 if (character.equals(GameEngine.getCharacter("Boat"))) {
                     GameEngine.getInstance().win();
-
+                
+                 //check if this collision is between an obstacle and the boat
                 } else {
                     character.collide();
                 }
             }
 
         }
+        //check if the collision is a possible option
         assert(collision == false || collision == true) : "collision is invalid!";
         return collision;
     }
